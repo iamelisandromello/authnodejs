37e8724abeff815fcc2cddfbaa00cab19f75976b
Elisandro <elisandro@ulbra.edu.br>
2020-01-27 12:47:22 -0300

    Dependence Mongoose and  Database Connection

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
+
 require('dotenv').config({
    path:'variables.env'
+});
+
+//DATABASE CONNECTION
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise; //INFORMA AO BD QUE PODE UTILIZAR ECMASCRIPT6 [PROMISSE, ASYNC...]
+mongoose.connection.on('error', (error)=>{
+   console.error("Erro: " + error.message);
 });
 
 const app = require('./app');
